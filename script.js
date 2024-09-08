746810b1c3adcba01580829cf119b1e493f66a0e
Jaes98 <marcus.jaes@gmail.com>
2024-09-08 21:29:36 +0200

    del 2

--- script.js
+++ script.js
@@ -1,21 +1,52 @@
 window.addEventListener("load", start);
 
+let guess;
 let guessCount = 0;
 let currentGuess;
+let previousGuess;
+let attemptMessage;
+let min = 1;
+let max = 100;
+let middle = Math.floor((max + min) / 2);
 
 function start() {
   console.log("JS kører");
-  makeGuess();
+
+  attemptMessage = document.querySelector("#attemptMessage");
+
+  playButton = document.querySelector("#play-btn");
+  playButton.addEventListener("click", startGame);
+
+}
+
+function startGame() {
+  attemptMessage.textContent = "";
+  guessCount = 0;
+  min = 1;
+  max = 100;
 
   document.querySelector("#tooLowButton").addEventListener("click", tooLow);
   document.querySelector("#tooHighButton").addEventListener("click", tooHigh);
   document.querySelector("#correctButton").addEventListener("click", correct);
+
+  makeNewGuess();
 }
 
-function makeGuess() {
-  currentGuess = getRandomGuess();
-  guessCount++;
-  outputGuess(currentGuess);
+function makeNewGuess() {
+  if (min <= max) {
+    const offset =  
+    Math.floor(Math.random() * ((max - min) * 0.2)) - (max - min) * 0.1;
+
+    guess = Math.floor((max + min) / 2 + offset);
+
+    previousGuess = guess;
+    guessCount++;
+    outputGuess(guess);
+    document.querySelector("#output").textContent= `Gætter på: ${guess}`;
+  } else {
+    attemptMessage.textContent = `Jeg gættede på ${lastGuess} - Der er ikke flere gæt`;
+    console.error("Error: No more guesses");
+  }
 }
 
 function getRandomGuess() {
@@ -31,19 +62,38 @@ function outputGuess(guess) {
 
 function tooLow() {
   console.log("For lavt");
-  makeGuess();
+  min = guess + 1
+  makeNewGuess();
 }
 
 function tooHigh() {
   console.log("For højt");
-  makeGuess();
+
+  max = guess - 1;
+
+  makeNewGuess();
 }
 
 function correct() {
   console.log("Korrekt");
   const output = document.querySelector("#output");
-  output.textContent = `Jeg gættede rigtigt! Dit tal var ${currentGuess}.`;
+  output.textContent = `Jeg gættede rigtigt!`;
+  attemptMessage.textContent = `${getCommentForAttempts(guessCount)}`;
   turnOffButtons();
+}
+
+function getCommentForAttempts(attempts) {
+  if (attempts <= 3) {
+    return "Du er en maskine";
+  } else if (attempts <= 5) {
+    return "Godt gættet";
+  } else if (attempts <= 7) {
+    return "Middelmådigt";
+  } else if (attempts > 7) {
+    return "Du skal øve dig";
+  } else {
+    return "";
+  }
 }
 
 function turnOffButtons() {
